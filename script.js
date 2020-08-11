2a2bf59efb1ef61067d70bd9c317dd9c080a4fd3
ankitt8 <ankit90499@gmail.com>
2020-08-11 22:01:33 +0530

    added feedback of wrong and right ans

--- script.js
+++ script.js
@@ -10,18 +10,41 @@ var ansKey = {
 }
 var submittedAns = {}
 document.getElementById('qn_ans_form').onclick = (e) => {
-    console.log(e.target)
+    // console.log(e.target)
     submittedAns[e.target.name] = e.target.value;
 }
 document.getElementById('qn_ans_form').onsubmit = (e) => {
     e.preventDefault();
     // to get the answers selected
     let score = 0;
-    Object.keys(ansKey).forEach(key => {
-        if (submittedAns[key] == ansKey[key]) {
+    let allInputElements = document.getElementsByTagName('input');
+    Object.keys(ansKey).forEach(qn => {
+        if (submittedAns[qn] == ansKey[qn]) {
+            for (let i = 0; i < allInputElements.length; ++i) {
+                const inputEle = allInputElements[i];
+                if (inputEle.name == qn) {
+                    if (inputEle.value == submittedAns[qn]) {
+                        console.log(inputEle)
+                        console.log(inputEle.parentElement)
+                        inputEle.parentElement.classList.add('green')
+                    }
+                }
+            }
+
             score += 1;
+        } else {
+            for (let i = 0; i < allInputElements.length; ++i) {
+                const inputEle = allInputElements[i];
+                if (inputEle.name == key) {
+                    if (inputEle.value == submittedAns[key]) {
+                        console.log(inputEle)
+                        console.log(inputEle.parentElement)
+                        inputEle.parentElement.classList.add('red')
+                    }
+                }
+            }
         }
     })
-    document.getElementById('score').innerHTML = `Your Score is: ${score}`;
+    document.getElementById('score').innerHTML = `Your Score is: ${score}/7`;
     console.log(score)
 }
