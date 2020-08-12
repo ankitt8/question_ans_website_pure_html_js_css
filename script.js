4da8f1a0d766fac0f26ccc1dd3d5877ff2612fd3
ankitt8 <ankit90499@gmail.com>
2020-08-12 10:47:12 +0530

    modified logic based on json structure

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
 var ansKey = {}
+var submittedAns = {}
+
 function displayQuestions(testId = 'physics_12-08-2020') {
-
-
-    // fetch the json for particular testId
-    
     fetch('./data/question_bank/physics_12-08-2020.json')
         .then(res => res.json())
         .then(res => {
+            const qnAnswers = res['questions_and_answerkey'];
+
             const formElement = document.getElementById('qn_ans_form');
             const fragment = document.createDocumentFragment();
-            for (let i = 0; i < res.length; ++i) {
-                ansKey[res[i]['qn_num']] = res[i]['answer'];
-                
+            for (const qnAnswer of qnAnswers) {
+                ansKey[qnAnswer['qn_num']] = qnAnswer['answer'];
                 // create ansKey object which will be used to validate the answers..
                 // const cardDivFragment = document.createDocumentFragment();
                 const cardDiv = document.createElement('div');
                 cardDiv.classList.add('card');
                 const qnTitlePTag = document.createElement('p');
                 qnTitlePTag.classList.add('qnTitle');
-                qnTitlePTag.innerHTML = `<strong>Q${res[i]['qn_num']})</strong> ${res[i]['qn_title']}`;
-
+                qnTitlePTag.innerHTML = `<strong>Q${qnAnswer['qn_num']})</strong> ${qnAnswer['qn_title']}`;
                 // cardDivFragment.appendChild(cardDiv);
                 cardDiv.appendChild(qnTitlePTag);
 
                 const labelsFragment = document.createDocumentFragment();
-                for (const option of Object.keys(res[i]['options'])) {
+                for (const option of Object.keys(qnAnswer['options'])) {
 
                     const label = document.createElement('label');
-                    label.innerHTML = `<input type="radio" name=${res[i]['qn_num']} value=${option}> ${res[i]['options'][option]}`
+                    label.innerHTML = `<input type="radio" name=${qnAnswer['qn_num']} value=${option}> ${qnAnswer['options'][option]}`
                     labelsFragment.appendChild(label);
 
                 }
@@ -49,34 +47,20 @@ function displayQuestions(testId = 'physics_12-08-2020') {
             inputSubmitContainerFrag.appendChild(pScoreEle)
             inputSubmitContainer.appendChild(inputSubmitContainerFrag);
             fragment.appendChild(inputSubmitContainer)
-  
+
             formElement.appendChild(fragment);
-
         });
-
 }
 displayQuestions(testId = 'physics_12-08-2020');
 
-// var ansKey = {
-//     q1: 'c',
-//     q2: 'd',
-//     q3: 'a',
-//     q4: 'c',
-//     q5: 'a',
-//     q6: 'd',
-//     q7: 'd'
-// }
-var submittedAns = {}
-
 // store the ansewers clicked during the test
 document.getElementById('qn_ans_form').onclick = (e) => {
-    
     submittedAns[e.target.name] = e.target.value;
 }
 document.getElementById('qn_ans_form').onsubmit = (e) => {
     e.preventDefault();
     // to get the answers selected
-    console.log(ansKey)
+
     let score = 0;
     let allInputElements = document.getElementsByTagName('input');
     Object.keys(ansKey).forEach(qn => {
@@ -85,8 +69,6 @@ document.getElementById('qn_ans_form').onsubmit = (e) => {
                 const inputEle = allInputElements[i];
                 if (inputEle.name == qn) {
                     if (inputEle.value == submittedAns[qn]) {
-                        console.log(inputEle)
-                        console.log(inputEle.parentElement)
                         inputEle.parentElement.classList.add('green')
                     }
                 }
@@ -98,8 +80,6 @@ document.getElementById('qn_ans_form').onsubmit = (e) => {
                 const inputEle = allInputElements[i];
                 if (inputEle.name == qn) {
                     if (inputEle.value == submittedAns[qn]) {
-                        console.log(inputEle)
-                        console.log(inputEle.parentElement)
                         inputEle.parentElement.classList.add('red')
                     } else {
                         if (inputEle.value == ansKey[qn]) {
@@ -111,6 +91,8 @@ document.getElementById('qn_ans_form').onsubmit = (e) => {
         }
     })
     document.getElementById('score').innerHTML = `Your Score is: ${score}/7`;
-    console.log(score)
 }
+
+// to display a timer
+
 
