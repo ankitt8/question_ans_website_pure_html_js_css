82b111a4c56ece3a403df634f6a0c8123b339016
ankitt8 <ankit90499@gmail.com>
2020-08-15 00:00:32 +0530

    bug fixing 1

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var maxTimeInMinutes = 0;
 var totalQuestions = 0;
 var data;
 var score;
+// var qnAnswer
 // async function getQuestionBank(testId = 'physics_12-08-2020.json') {
 //     let response  = await fetch('./data/question_bank/' + testId);
 //     let data = await response.json();
@@ -15,97 +16,123 @@ var score;
 // })
 // // const data  = .then((data) => data);
 // console.log(data)
+document.querySelector('#submitTestModalId').style.display = 'none';
+
+function showQuestions() {
+    for (const qnAnswer of qnAnswers) {
+        ansKey[qnAnswer['qn_num']] = qnAnswer['answer'];
+        // create ansKey object which will be used to validate the answers..
+        // const cardDivFragment = document.createDocumentFragment();
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('card');
+        const qnTitleTag = document.createElement('h2');
+        qnTitleTag.classList.add('qnTitle');
+        qnTitleTag.innerHTML = `<strong>Q${qnAnswer['qn_num']})</strong> ${qnAnswer['qn_title']}`;
+        // cardDivFragment.appendChild(cardDiv);
+        cardDiv.appendChild(qnTitleTag);
+
+        const labelsFragment = document.createDocumentFragment();
+        for (const option of Object.keys(qnAnswer['options'])) {
+
+            const label = document.createElement('label');
+            label.innerHTML = `<input type="radio" name=${qnAnswer['qn_num']} value=${option}> ${qnAnswer['options'][option]}`
+            labelsFragment.appendChild(label);
+
+        }
+        cardDiv.appendChild(labelsFragment);
+        fragment.appendChild(cardDiv);
+    }
+    const inputSubmitContainer = document.createElement('div');
+    inputSubmitContainer.classList.add('submit-container')
+    const inputSubmitContainerFrag = document.createDocumentFragment();
+
+    const inputSubmitElem = document.createElement('input')
+    inputSubmitElem.classList.add('btn')
+    inputSubmitElem.setAttribute('type', 'submit');
+    inputSubmitElem.setAttribute('value', 'Submit');
+
+    inputSubmitContainerFrag.appendChild(inputSubmitElem);
+
+
+    inputSubmitContainer.appendChild(inputSubmitContainerFrag);
+    fragment.appendChild(inputSubmitContainer)
+
+    formElement.appendChild(fragment);
+
+    formElement.addEventListener('click', (e) => {
+        if (e.target.value != 'Submit') {
+            submittedAns[e.targeti.name] = e.target.value;
+
+        }
+    })
+}
 function displayQuestions(testId = 'physics_12-08-2020.json') {
     fetch('./data/question_bank/' + testId)
         .then(res => res.json())
         .then(res => {
             maxTimeInMinutes = parseInt(res['timerInMinutes'])
-            const qnAnswers = res['questions_and_answerkey'];
+            qnAnswers = res['questions_and_answerkey'];
             totalQuestions = qnAnswers.length;
+
+            createStartTestModal();
             document.getElementsByClassName('highlight')[0].innerText = `${res['title']} `;
-            const formElement = document.getElementById('qn_ans_form');
-            const fragment = document.createDocumentFragment();
-            for (const qnAnswer of qnAnswers) {
-                ansKey[qnAnswer['qn_num']] = qnAnswer['answer'];
-                // create ansKey object which will be used to validate the answers..
-                // const cardDivFragment = document.createDocumentFragment();
-                const cardDiv = document.createElement('div');
-                cardDiv.classList.add('card');
-                const qnTitleTag = document.createElement('h2');
-                qnTitleTag.classList.add('qnTitle');
-                qnTitleTag.innerHTML = `<strong>Q${qnAnswer['qn_num']})</strong> ${qnAnswer['qn_title']}`;
-                // cardDivFragment.appendChild(cardDiv);
-                cardDiv.appendChild(qnTitleTag);
 
-                const labelsFragment = document.createDocumentFragment();
-                for (const option of Object.keys(qnAnswer['options'])) {
+            formElement = document.getElementById('qn_ans_form');
+            fragment = document.createDocumentFragment();
+            document.querySelector('#startTestBtn').addEventListener('click', () => {
+                document.querySelector('#startTestModalId').style.display = 'none';
+                // document.querySelector('.timer').style.display = 'inline-block';
+                showQuestions();
+                createTimer(maxTimeInMinutes, formElement);
+                formElement.onsubmit = handleFormSubmit;
 
-                    const label = document.createElement('label');
-                    label.innerHTML = `<input type="radio" name=${qnAnswer['qn_num']} value=${option}> ${qnAnswer['options'][option]}`
-                    labelsFragment.appendChild(label);
-
-                }
-                cardDiv.appendChild(labelsFragment);
-                fragment.appendChild(cardDiv);
-            }
-            const inputSubmitContainer = document.createElement('div');
-            inputSubmitContainer.classList.add('submit-container')
-            const inputSubmitContainerFrag = document.createDocumentFragment();
-
-            const inputSubmitElem = document.createElement('input')
-            inputSubmitElem.classList.add('btn')
-            inputSubmitElem.setAttribute('type', 'submit');
-            inputSubmitElem.setAttribute('value', 'Submit');
-            inputSubmitElem.addEventListener('click', createSubmitTestModal);
-            inputSubmitContainerFrag.appendChild(inputSubmitElem);
+            })
 
 
-            inputSubmitContainer.appendChild(inputSubmitContainerFrag);
-            fragment.appendChild(inputSubmitContainer)
 
-            formElement.appendChild(fragment);
-            createTimer(maxTimeInMinutes, formElement);
+
+
         });
 }
 
 function createStartTestModal() {
     document.querySelector('#startTestModalId').style.display = 'block';
-    // document.querySelector('#numOfQns').innerText = Object.keys(ansKey).length;
-    // document.querySelector('#duration').innerText = maxTimeInMinutes;
-    document.querySelector('#startTestBtn').addEventListener('click', () => {
-        document.querySelector('#startTestModalId').style.display = 'none';
-        // document.querySelector('.timer').style.display = 'inline-block';
-        displayQuestions(testId = 'physics_12-08-2020.json');
-    })
+    document.querySelector('#numOfQnsStartTestModal').innerText = totalQuestions;
+    document.querySelector('#duration').innerText = maxTimeInMinutes;
+}
 
+function handleFormSubmit() {
+    unattemptedQns = 0;
+    score = calculateScore();
+    document.getElementById('score').innerHTML = `Score <span class="score">${score}/${totalQuestions}</span>`;
+
+    // createSubmitTestModal();
 }
 function createSubmitTestModal() {
     console.log('inside createSubmitTestModal')
     document.querySelector('#submitTestModalId').style.display = 'block';
-    // document.querySelector('#correctAns').innerText = score;
+    document.querySelector('#correctAns').innerText = score;
+    document.querySelector('#wrongAns').innerText = totalQuestions - score - unattemptedQns;
+    document.querySelector('#unattemptedQns').innerText = unattemptedQns;
     document.querySelector('#submitTestModalClose').addEventListener('click', () => {
         document.querySelector('#submitTestModalId').style.display = 'none';
-       
     })
+
     // disable all input 
     // document.querySelectorAll('input').forEach(ele => {ele.disabled = true;})
     // document.querySelector('#wrongAns').innerText = 
 }
-window.onload = function () {
+// window.onload = function () {
 
-    document.querySelector('#submitTestModalId').style.display = 'none';
-    createStartTestModal();
+//     createStartTestModal();
 
-}
+// }
 
 // store the ansewers clicked during the test
-document.getElementById('qn_ans_form').onclick = (e) => {
-    submittedAns[e.target.name] = e.target.value;
-}
 function calculateScore(e) {
     // console.log(e)
     clearInterval(timerId)
-    e.preventDefault();
+    // e.preventDefault();
     // to get the answers selected
 
     score = 0;
@@ -137,9 +164,9 @@ function calculateScore(e) {
             }
         }
     })
-    document.getElementById('score').innerHTML = `Score <span class="score">${score}/${totalQuestions}</span>`;
+    return score;
+
 }
-document.getElementById('qn_ans_form').onsubmit = calculateScore;
 
 function createTimer(maxTimeInMinutes, formElement) {
     maxTimeInMinutes = parseInt(maxTimeInMinutes)
@@ -185,5 +212,5 @@ function createTimer(maxTimeInMinutes, formElement) {
     // setTimeout(() => {clearInterval(timerId)}, maxTimeInMinutes*60*1000);
 }
 
-
+displayQuestions(testId = 'physics_12-08-2020.json');
 
