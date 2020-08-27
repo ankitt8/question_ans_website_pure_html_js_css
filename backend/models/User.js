fb766226874e16942366a68728214d5edb513986
ankitt8 <ankit90499@gmail.com>
2020-08-27 20:48:09 +0530

    displaying rank

--- backend/models/User.js
+++ backend/models/User.js
@@ -19,6 +19,12 @@ const UserSchema = new mongoose.Schema({
     },
     score: {
         type: Number
+    },
+    timeTaken: {
+        type: Number
+    }, 
+    testId: {
+        type: String
     }
 })
 
